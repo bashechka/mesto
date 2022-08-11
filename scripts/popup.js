f65bbbc288f94cfa2d9162e009ab719a913bc168
Varvara Korobkina <ya.bashechka@yandex.ru>
2022-08-11 12:29:52 +0300

    LiveServer + Webpack

--- scripts/popup.js
+++
@@ -1,5 +0,0 @@
-const popupPhoto = document.querySelector('.popup_type_open-pic');
-const pic = document.querySelector('.popup__photo');
-const caption = document.querySelector('.popup__photo-caption');
-
-export {popupPhoto, pic, caption};
